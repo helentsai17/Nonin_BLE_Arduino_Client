7123f78d87beb3372d9cd4257f8151b2baa0d4a0
VictorReal2022 <118308832+VictorReal2022@users.noreply.github.com>
2024-05-15 12:23:55 -0600

    successfully send to firebase

--- BLE_client_modify_v1.ino
+++ BLE_client_modify_v1.ino
@@ -1,5 +1,11 @@
 #include "BLEDevice.h"
 //#include "BLEScan.h"
+#include <WiFi.h>
+#include<Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#include <string>
+
 
 // The remote service we wish to connect to.
 static BLEUUID    SPO2serviceUUID("46a970e0-0d5f-11e2-8b5e-0002a5d5c51b");
@@ -10,75 +16,79 @@ static BLEUUID    HeartRateCharacterUUID("2A37");
 // static BLEUUID    PPGCharacterUUID("ee0a883a-4d24-11e7-b114-b2f933d5fe66");
 // static BLEUUID    PulseIntervalCharacterUUID("34e27863-76ff-4f8e-96f1-9e3993aa6199");
 
-
 static boolean doConnect = false; // can and find the device
 static boolean connected = false;
 static boolean doScan = false;
-
 static BLERemoteCharacteristic* SPO2RemoteCharacteristic;
 static BLERemoteCharacteristic* HeartRateRemoteCharacteristic;
 // static BLERemoteCharacteristic* PulseIntervalRemoteCharacteristic;
-
 static BLEAdvertisedDevice* myDevice;
 
-int BLEConnectLED = 23;
-int FirebaseConnectSuccess = 16;
-int restartBLEScan = 13;
 
 
+// FirebaseData
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+//wifi login
+bool Wifi_connect = false;
 
+// const char* MachineID = "Nonin";
+std::string MachineID = "Nonin";
+
+int spo2Data = 0;
+int heartRateData = 0;
 
 static void notifyCallback(
   BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData,size_t length,bool isNotify) {
-    // Serial.print("Notify callback for characteristic ");
-    // Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
-    // Serial.print(" of data length ");
-    // Serial.println(length);
     if(pBLERemoteCharacteristic->getUUID().toString() == SPO2CharacterUUID.toString()){
-      Serial.print("SPO2: ");
+      Serial.print(F("SPO2: "));
       Serial.println(pData[7]);
+      spo2Data = pData[7];
     }
     // else if(pBLERemoteCharacteristic->getUUID().toString() == PulseIntervalCharacterUUID.toString()){
     //   Serial.print("Pulse Interval: ");
     //   Serial.println(pData[5]);
     // }
     else if(pBLERemoteCharacteristic->getUUID().toString() == HeartRateCharacterUUID.toString()){
-      Serial.print("Heart Rate: ");
+      Serial.print(F("Heart Rate: "));
       Serial.println(pData[1]);
+      heartRateData = pData[1];
     }
  
 }
 
 class MyClientCallback : public BLEClientCallbacks {
   void onConnect(BLEClient* pclient) {
-    Serial.println("on connect Called");
+    // Serial.println("on connect Called");
   }
-
   void onDisconnect(BLEClient* pclient) {
     connected = false;
-    digitalWrite(BLEConnectLED, LOW);
-    Serial.println("onDisconnect");
+    digitalWrite(23, LOW);
+    digitalWrite(18, LOW);
+    Serial.println(F("onDisconnect"));
   }
 };
 
 
 bool connectToServer() {
-    Serial.print("Forming a connection to ");
-    Serial.println(myDevice->getAddress().toString().c_str());
-
+    // Serial.print("Forming a connection to ");
+    // Serial.println(myDevice->getAddress().toString().c_str());
     // this is use for pairing
     BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT);
   
     delay(3000);
     
     BLEClient*  pClient  = BLEDevice::createClient();
-    Serial.println(" - Created client");
+    Serial.println(F(" - Created client"));
 
     pClient->setClientCallbacks(new MyClientCallback());
 
     // Connect to the remove BLE Server.
     pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-    Serial.println(" - Connected to server");
+    Serial.println(F(" - Connected to server"));
 
     // Obtain a reference to the service we are after in the remote BLE server.
     BLERemoteService* pRemoteService = pClient->getService(SPO2serviceUUID);
@@ -95,7 +105,7 @@ bool connectToServer() {
       pClient->disconnect();
       return false;
     }
-    Serial.println(" - Found our service: ");
+    Serial.println(F(" - Found our service: "));
 
     connected = true;
 
@@ -116,11 +126,11 @@ bool connectToServer() {
 
     if(connected == false) {
       pClient-> disconnect();
-      Serial.println("At least one characteristic UUID not found");
+      // Serial.println(F("At least one characteristic UUID not found"));
       return false;
     }
 
-    digitalWrite(BLEConnectLED, HIGH);
+    digitalWrite(23, HIGH);
     return true;
 }
 
@@ -131,29 +141,13 @@ bool connectCharacteristic(BLERemoteService* pRemoteService, BLERemoteCharacteri
     Serial.print(l_BLERemoteChar->getUUID().toString().c_str());
     return false;
   }
-  Serial.println(" - Found characteristic: " + String(l_BLERemoteChar->getUUID().toString().c_str()));
+  // Serial.println(" - Found characteristic: " + String(l_BLERemoteChar->getUUID().toString().c_str()));
 
   if(l_BLERemoteChar->canNotify())
     l_BLERemoteChar->registerForNotify(notifyCallback);
 
   return true;
 }
-
-bool connectService(BLEClient*  pClient, BLERemoteService* pRemoteService) {
-  // Obtain a reference to the characteristic in the service of the remote BLE server.
-  if (pRemoteService == nullptr) {
-    Serial.print("Failed to find one of the characteristics");
-    Serial.print(pRemoteService->getUUID().toString().c_str());
-    return false;
-  }
-  Serial.println(" - Found service: " + String(pRemoteService->getUUID().toString().c_str()));
-
-  return true;
-}
-
-
-
-
 
 /**
  * Scan for BLE servers and find the first one that advertises the service we are looking for.
@@ -169,7 +163,11 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     // We have found a device, let us now see if it contains the service we are looking for.
     if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(SPO2serviceUUID)) {
       Serial.print("BLE Advertised Device found: ");
-      Serial.println(advertisedDevice.toString().c_str());
+      // Serial.println(advertisedDevice.toString().c_str());
+      Serial.println(advertisedDevice.getName().c_str());
+
+      MachineID =  std::string(advertisedDevice.getName().c_str());
+      // MachineID = advertisedDevice.getName().c_str();
 
       BLEDevice::getScan()->stop();
       myDevice = new BLEAdvertisedDevice(advertisedDevice);
@@ -180,17 +178,30 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   } // onResult
 }; // MyAdvertisedDeviceCallbacks
 
+void sighInToFirebase(){
 
-void ScanToFindNonin(){
-   // Retrieve a Scanner and set the callback we want to use to be informed when we
-  // have detected a new device.  Specify that we want active scanning and start the
-  // scan to run for 5 seconds.
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setInterval(1349);
-  pBLEScan->setWindow(449);
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(5, false);
+
+  //https://www.youtube.com/watch?v=aO92B-K4TnQ
+  //firebase login
+    config.api_key = API_KEY;
+    config.database_url = DATABASE_URL;
+
+   
+    if(Firebase.signUp(&config, &auth, "", "")){
+      // Serial.println("SignUp OK");
+      signupOK = true;
+    }else{
+      //Serial.println("%s\n", config.signer.signupError.message.c_str());
+    }
+
+
+    config.token_status_callback = tokenStatusCallback;
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
+
+
+    // start led when wificonnection is up and firebase is sigh in
+    digitalWrite(18, HIGH);
 }
 
 
@@ -198,11 +209,28 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting Arduino BLE Client application...");
   BLEDevice::init("");
-  pinMode(BLEConnectLED, OUTPUT);
-  pinMode(restartBLEScan, INPUT);
+  pinMode(23, OUTPUT);
+  pinMode(18, OUTPUT);
 
-  ScanToFindNonin();
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
 
+  //connect to Wifi
+  WiFi.begin("ULink", "u604947263z243");
+  Serial.println("Connecting to WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+    Wifi_connect = true;
+  }
+  Serial.println("Connected with IP: ");
+  Serial.print(WiFi.localIP());
+  
+  
  
 } // End of setup.
 
@@ -210,30 +238,65 @@ void setup() {
 // This is the Arduino main loop function.
 void loop() {
 
-  if(digitalRead(restartBLEScan) == HIGH){
-    ScanToFindNonin();
+  if(Wifi_connect && !signupOK ){
+    sighInToFirebase();
   }
+
 
   // If the flag "doConnect" is true then we have scanned for and found the desired
   // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
   // connected we set the connected flag to be true.
   if (doConnect == true) {
     if (connectToServer()) {
-      Serial.println("We are now connected to the BLE Server.");
+      // Serial.println("We are now connected to the BLE Server.");
     } else {
-      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+      // Serial.println("We have failed to connect to the server; there is nothin more we will do.");
     }
     doConnect = false;
   }
 
   // If we are connected to a peer BLE Server, update the characteristic each time we are reached
   // with the current time since boot.
-  if (connected) {
+  if (connected && Firebase.ready() && signupOK) {
     String newValue = "Time since boot: " + String(millis()/1000);
     Serial.println("Setting new characteristic value to \"" + newValue + "\"");
+
+    std::string device = "NoninData/"+ MachineID + "/device";
+    std::string heartRatePath = "NoninData/"+ MachineID + "/HeartRate";
+    std::string spo2Path = "NoninData/"+ MachineID + "/SPO2";
+
+
+    if(Firebase.RTDB.setString(&fbdo, heartRatePath, heartRateData)){
+      // Serial.println();
+      // Serial.print(MachineID);
+      Serial.print(" - successfully save to: " + fbdo.dataPath());
+      Serial.println("( " + fbdo.dataType() + ")");
+    }else{
+      Serial.println("Falled to send machine name: " + fbdo.errorReason());
+    }
+
+    if(Firebase.RTDB.setString(&fbdo, spo2Path, spo2Data)){
+      // Serial.println();
+      // Serial.print(MachineID);
+      Serial.print(" - successfully save to: " + fbdo.dataPath());
+      Serial.println("( " + fbdo.dataType() + ")");
+    }else{
+      Serial.println("Falled to send machine name: " + fbdo.errorReason());
+    }
+
+
+    // if(Firebase.RTDB.setString(&fbdo, Speedstr, speedData)){
+    //   Serial.println();
+    //   Serial.print(speedData);
+    //   Serial.print(" - successfully save to: " + fbdo.dataPath());
+    //   Serial.println("( " + fbdo.dataType() + ")");
+    // }else{
+    //   Serial.println("Falled to send speed: " + fbdo.errorReason());
+    // }
     
     // Set the characteristic's value to be the array of bytes that is actually a string.
     // SPO2RemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+    // sendDataPrevMillis = millis();
   }else if(doScan){
     BLEDevice::getScan()->start(0);  // this is just eample to start scan after disconnect, most likely there is better way to do it in arduino
   }
